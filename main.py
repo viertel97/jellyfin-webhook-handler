72d3508c5c759f510ae3fa699df914936aa136cd
Janik Viertel <janik@next-data-service.com>
2024-08-18 00:42:33 +0200

    added health check

--- main.py
+++ main.py
@@ -50,5 +50,10 @@ async def webhook(request: Request):
         raise HTTPException(status_code=404, detail="Series not found")
 
 
+# health check
+@app.get("/")
+def read_root():
+    return {"status": "ok"}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
