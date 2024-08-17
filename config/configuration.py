132618897c2ec81d26a4938ec35ef539a2282c71
Janik Viertel <janik@next-data-service.com>
2024-08-18 00:33:49 +0200

    changed secret

--- config/configuration.py
+++ config/configuration.py
@@ -1,5 +1,5 @@
 import os
 from quarter_lib.akeyless import get_secrets
 
-API_KEY = get_secrets(["sonarr/api-key"])
+API_KEY = get_secrets(["sonarr/api_key"])
 SONARR_URL = os.getenv("SONARR_URL", "http://localhost:8989")
